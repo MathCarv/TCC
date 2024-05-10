0a009a77e9e809895e01f33079b4786f863a3b04
matheus <matheus.camara@gec.inatel.br>
2024-05-09 23:08:46 -0300

    V1.5

--- scripts/database.js
+++ scripts/database.js
@@ -1,35 +1,41 @@
-const sql = require('mssql');
+// database.js
 
-const config = {
-  user: 'CloudSA76d82763', // Seu usuário administrador do servidor
-  password: 'Tcc2024*', // Sua senha de administrador do servidor
-  server: 'usuarios.database.windows.net', // Nome do servidor SQL
-  database: 'users', // Nome do banco de dados
-  options: {
-    encrypt: true, // Usar criptografia
-    trustServerCertificate: false // Não confiar automaticamente no certificado do servidor
+const { Sequelize, DataTypes } = require('sequelize');
+
+// Configurar a conexão com o banco de dados
+const sequelize = new Sequelize('users', 'CloudSA76d82763', 'Tcc2024*', {
+  host: 'usuarios.database.windows.net',
+  dialect: 'mssql',
+  dialectOptions: {
+    encrypt: true,
+    trustServerCertificate: false
   }
-};
+});
 
-// Função para realizar uma consulta no banco de dados
-async function executeQuery(query) {
+// Definir o modelo de usuário
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+// Sincronizar o modelo com o banco de dados
+(async () => {
   try {
-    await sql.connect(config);
-    const result = await sql.query(query);
-    return result.recordset;
-  } catch (err) {
-    console.error('Erro ao executar consulta:', err);
-    throw err;
-  } finally {
-    await sql.close();
+    await sequelize.authenticate();
+    console.log('Conexão bem-sucedida com o banco de dados.');
+    await sequelize.sync(); // Isso sincronizará o modelo com o banco de dados, criando a tabela se não existir
+    console.log('Modelo sincronizado com o banco de dados.');
+  } catch (error) {
+    console.error('Erro ao conectar e sincronizar o modelo:', error);
   }
-}
+})();
 
-// Exemplo de uso da função executeQuery
-executeQuery('SELECT * FROM Users')
-  .then((data) => {
-    console.log('Dados recuperados do banco de dados:', data);
-  })
-  .catch((err) => {
-    console.error('Erro ao recuperar dados do banco de dados:', err);
-  });
+// Exportar o modelo de usuário
+module.exports = { User };
--- server.js
+++ server.js
@@ -1,32 +1,14 @@
+// server.js
+
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const path = require('path');
-const EventHubReader = require('./scripts/event-hub-reader.js');
 const db = require('./scripts/database.js');
 
-const iotHubConnectionString = process.env.IotHubConnectionString;
-if (!iotHubConnectionString) {
-  console.error(`Environment variable IotHubConnectionString must be specified.`);
-  return;
-}
-console.log(`Using IoT Hub connection string [${iotHubConnectionString}]`);
-
-const eventHubConsumerGroup = process.env.EventHubConsumerGroup;
-console.log(eventHubConsumerGroup);
-if (!eventHubConsumerGroup) {
-  console.error(`Environment variable EventHubConsumerGroup must be specified.`);
-  return;
-}
-console.log(`Using event hub consumer group [${eventHubConsumerGroup}]`);
-
-// Inicialize o aplicativo Express
 const app = express();
 
-// Configurar a pasta de arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Adicionar middlewares para analisar dados do corpo da solicitação
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -58,10 +40,6 @@ app.post('/login', async (req, res) => {
 
 // Rota para lidar com a solicitação de logout
 app.get('/logout', (req, res) => {
-  // Aqui você pode limpar a sessão do usuário, se estiver usando sessões
-  // Por exemplo: req.session.destroy();
-
-  // Redirecione o usuário de volta para a página de login
   res.redirect('/login');
 });
 
@@ -70,46 +48,12 @@ app.use((req, res) => {
   res.redirect('/login');
 });
 
-// Criar o servidor HTTP
 const server = http.createServer(app);
-
-// Inicializar o servidor WebSocket
 const wss = new WebSocket.Server({ server });
 
-wss.broadcast = (data) => {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      try {
-        console.log(`Broadcasting data ${data}`);
-        client.send(data);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  });
-};
+// Código do WebSocket aqui...
 
-// Iniciar o servidor HTTP
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// Inicializar o leitor do Event Hub
-const eventHubReader = new EventHubReader(iotHubConnectionString, eventHubConsumerGroup);
-
-(async () => {
-  await eventHubReader.startReadMessage((message, date, deviceId) => {
-    try {
-      const payload = {
-        IotData: message,
-        MessageDate: date || Date.now().toISOString(),
-        DeviceId: deviceId,
-      };
-
-      wss.broadcast(JSON.stringify(payload));
-    } catch (err) {
-      console.error('Error broadcasting:', err);
-    }
-  });
-})().catch(console.error);
