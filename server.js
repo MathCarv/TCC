7e05ca169951705ef1779db5f969fe99cfe394a6
matheus <matheus.camara@gec.inatel.br>
2024-05-11 18:49:27 -0300

    V1.8

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-// server.js
-
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const path = require('path');
-const db = require('./scripts/database.js');
+const fs = require('fs');
 
 const app = express();
 
@@ -12,30 +10,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Caminho para o arquivo onde as credenciais serão armazenadas
+const credentialsFile = path.join(__dirname, 'credentials.json');
+
 // Rota para a página de login
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 // Rota para autenticar o usuário
-app.post('/login', async (req, res) => {
+app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  try {
-    // Consultar o banco de dados para verificar as credenciais do usuário
-    const user = await db.User.findOne({ where: { username: username } });
-    if (!user || user.password !== password) {
+  // Carrega as credenciais do arquivo JSON
+  fs.readFile(credentialsFile, (err, data) => {
+    if (err) {
+      console.error('Error reading credentials file:', err);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+
+    const credentials = JSON.parse(data);
+
+    // Verifica se as credenciais fornecidas correspondem às credenciais armazenadas
+    if (credentials[username] === password) {
+      // Credenciais corretas
+      // Você pode retornar um token de autenticação aqui ou redirecionar para a página principal
+      return res.status(200).json({ message: 'Login successful' });
+    } else {
       // Credenciais inválidas
       return res.status(401).json({ message: 'Invalid username or password' });
     }
-
-    // Credenciais corretas
-    // Você pode retornar um token de autenticação aqui ou redirecionar para a página principal
-    return res.status(200).json({ message: 'Login successful' });
-  } catch (error) {
-    console.error('Error authenticating user:', error);
-    return res.status(500).json({ message: 'Internal server error' });
-  }
+  });
 });
 
 // Rota para lidar com a solicitação de logout
@@ -60,7 +65,7 @@ wss.on('connection', (ws) => {
     console.log('Received message:', message);
 
     // Aqui você pode processar a mensagem recebida e responder ao cliente, se necessário
-    // Por exemplo, você pode consultar o banco de dados e enviar os dados para o cliente
+    // Por exemplo, você pode consultar o arquivo JSON e enviar os dados para o cliente
     // ws.send('Hello, client!');
   });
 
