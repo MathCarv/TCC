7f91e6e0f3723ac25155d58b06303e1399278719
matheus <matheus.camara@gec.inatel.br>
2024-05-16 18:46:44 -0300

    V1.9

--- public/login.js
+++ public/login.js
@@ -19,8 +19,14 @@ $(document).ready(function() {
             contentType: 'application/json',
             data: JSON.stringify({ username: username, password: password }),
             success: function(response) {
-                // Redireciona o usuário para a página principal se o login for bem-sucedido
-                window.location.href = '/index.html';
+                // Verifica se a resposta contém o redirecionamento para a página index.html
+                if (response.redirect) {
+                    window.location.href = response.redirect; // Redireciona para a página especificada na resposta
+                } else {
+                    console.error('Redirect URL not found in response.');
+                    // Em caso de redirecionamento não encontrado na resposta, exibe uma mensagem de erro genérica
+                    errorMessage.text('Error: Redirect URL not found.');
+                }
             },
             error: function(xhr, status, error) {
                 console.error('Error logging in:', error);
