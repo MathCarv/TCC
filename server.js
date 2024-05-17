b8e5a08e9cd8c8cbab01581c55ce246b3bab623e
matheus <matheus.camara@gec.inatel.br>
2024-05-17 14:08:52 -0300

    V1.9

--- server.js
+++ server.js
@@ -5,9 +5,26 @@ const path = require('path');
 const fs = require('fs');
 const session = require('express-session');
 const crypto = require('crypto');
+const EventHubReader = require('./scripts/event-hub-reader.js');
+
+
+const iotHubConnectionString = process.env.IotHubConnectionString;
+if (!iotHubConnectionString) {
+  console.error(`Environment variable IotHubConnectionString must be specified.`);
+  return;
+}
+console.log(`Using IoT Hub connection string [${iotHubConnectionString}]`);
+
+const eventHubConsumerGroup = process.env.EventHubConsumerGroup;
+console.log(eventHubConsumerGroup);
+if (!eventHubConsumerGroup) {
+  console.error(`Environment variable EventHubConsumerGroup must be specified.`);
+  return;
+}
+console.log(`Using event hub consumer group [${eventHubConsumerGroup}]`);
+
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -103,6 +120,36 @@ wss.on('connection', (ws) => {
     console.log('Client disconnected');
   });
 });
+
+wss.broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      try {
+        console.log(`Broadcasting data ${data}`);
+        client.send(data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+};
+
+const eventHubReader = new EventHubReader(iotHubConnectionString, eventHubConsumerGroup);
+(async () => {
+  await eventHubReader.startReadMessage((message, date, deviceId) => {
+    try {
+      const payload = {
+        IotData: message,
+        MessageDate: date || Date.now().toISOString(),
+        DeviceId: deviceId,
+      };
+
+      wss.broadcast(JSON.stringify(payload));
+    } catch (err) {
+      console.error('Error broadcasting: [%s] from [%s].', err, message);
+    }
+  });
+})().catch();
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
