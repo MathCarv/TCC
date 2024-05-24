35086c37c28b6dfc0a42f5143171fec536b34759
matheus <matheus.camara@gec.inatel.br>
2024-05-24 13:39:58 -0300

    V1.9

--- public/login.js
+++ public/login.js
@@ -28,26 +28,24 @@ $(document).ready(function() {
                     window.location.href = response.redirect;
                 } else {
                     console.error('Redirect URL not found in response.');
-                    // Vulnera a XSS ao inserir diretamente HTML sem sanitização
-                    errorMessage.html('Error: ' + response.message);
+                    errorMessage.html('Error: Redirect URL not found.');
                 }
             },
             error: function(xhr, status, error) {
                 console.error('Error logging in:', error); // Log do erro
-                // Vulnera a XSS ao inserir diretamente HTML sem sanitização
                 errorMessage.html('Nome de usuário ou senha inválidos. Por favor, tente novamente.');
-
+                
                 // Centralizar a mensagem de erro abaixo do botão de login
                 const loginButton = $('#loginButton');
                 const loginButtonPosition = loginButton.offset();
                 const loginButtonHeight = loginButton.outerHeight();
                 const errorMessageHeight = errorMessage.outerHeight();
                 const errorMessageWidth = errorMessage.outerWidth();
-
+                
                 // Calcula a posição correta para a mensagem de erro
                 const errorMessageTop = loginButtonPosition.top + loginButtonHeight + 10;
                 const errorMessageLeft = (loginButtonPosition.left + loginButton.outerWidth() / 2) - (errorMessageWidth / 2);
-
+                
                 // Define a posição da mensagem de erro
                 errorMessage.css({
                     position: 'absolute',
