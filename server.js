af6d68083403951f7350e27ff6f80ca0f1cacb73
matheus <matheus.camara@gec.inatel.br>
2024-06-24 15:18:05 -0300

    V1.3

--- server.js
+++ server.js
@@ -151,3 +151,5 @@ const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app; // Exporta o app para uso nos testes
---
+++ server.test.mjs
@@ -0,0 +1,52 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { expect } from 'chai';
+import server from './server.js';
+
+chai.use(chaiHttp);
+
+describe('Server Tests', () => {
+    it('Deve redirecionar para /login na rota /', (done) => {
+        chai.request(server)
+            .get('/')
+            .end((err, res) => {
+                expect(res).to.redirect;
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+    it('Deve retornar a página de login na rota /login', (done) => {
+        chai.request(server)
+            .get('/login')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res).to.be.html;
+                done();
+            });
+    });
+
+    it('Deve fazer login com credenciais válidas', (done) => {
+        const credentials = { username: 'TCC', password: 'tcc' }; // Ajuste as credenciais aqui
+        chai.request(server)
+            .post('/login')
+            .send(credentials)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.have.property('redirect', '/index.html');
+                done();
+            });
+    });
+
+    it('Não deve fazer login com credenciais inválidas', (done) => {
+        const credentials = { username: 'wrongUser', password: 'wrongPassword' };
+        chai.request(server)
+            .post('/login')
+            .send(credentials)
+            .end((err, res) => {
+                expect(res).to.have.status(401);
+                expect(res.body).to.have.property('message', 'Invalid username or password');
+                done();
+            });
+    });
+});
